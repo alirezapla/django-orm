cb8e5e740466a1d1d48d28b9690f89ebdef15e65
alirezapla <hajlialireza@gmail.com>
2022-03-21 17:00:20 +0330

    new query added

--- Finance/queries.py
+++ Finance/queries.py
@@ -1,5 +1,5 @@
 from email.mime import base
-from django.db.models import F, Sum, Count, Case, When
+from django.db.models import F, Sum, Count, Case, When, Min, Q, Max
 from .models import *
 
 
@@ -53,13 +53,43 @@ def query_7():
 
 
 def query_8():
-    # TODO
-    pass
+    _max = 1
+    departments = (
+        Project.objects.filter(
+            Q(estimated_end_time__date__gte=F("end_time__date"))
+            & Q(estimated_end_time__hour__gte=F("end_time__hour"))
+            & Q(estimated_end_time__minute__gte=F("end_time__minute"))
+        )
+        .values("department_id")
+        .annotate(Count("department_id"))
+    )
+    for department in departments:
+        print(department)
+        if _max < department["department_id__count"]:
+            _max = department["department_id__count"]
+            emply = department["department_id"]
+
+    print(Department.objects.filter(id=emply))
+    return Department.objects.filter(id=emply).first()
 
 
 def query_9(x):
-    # TODO
-    pass
+    min_late = 0
+    employees = (
+        Attendance.objects.filter(Q(in_time__hour__gt=x) & Q(in_time__minute__gt=0))
+        .values("employee", "in_time", "employee__account__username")
+        .annotate(late_count=Count("employee"))
+        # .order_by("employee__account__username")
+    )
+    # return employess
+    for i, employee in enumerate(employees):
+        print(employee)
+        if min_late >= employee["late_count"] or i == 0:
+            min_late = employee["late_count"]
+            emply = employee["employee"]
+
+    print(Employee.objects.filter(id=emply).values("id", "account__username"))
+    return Employee.objects.filter(id=emply).first()
 
 
 def query_10():
